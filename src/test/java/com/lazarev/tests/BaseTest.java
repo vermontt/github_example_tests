8f89a287523f5ebde729791078337a5db7d7678a
vermontt <lazar.evilgenius@yandex.ru>
2023-04-13 22:57:49 +0300

    added system properties

--- src/test/java/com/lazarev/tests/BaseTest.java
+++ src/test/java/com/lazarev/tests/BaseTest.java
@@ -29,7 +29,7 @@ public class BaseTest {
                 + authConfig.getLogin()
                 + ":"
                 + authConfig.getPassword()
-                + "@selenoid.autotests.cloud/wd/hub/";
+                + getProperty("URLRemoteBrowser");
     }
 
     @AfterAll
